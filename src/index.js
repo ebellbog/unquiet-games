a1071c665933c2b5618e877ecc532407260059e9
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2024-06-05 18:20:54 -0400

    Mobile tweaks

--- src/index.js
+++ src/index.js
@@ -49,7 +49,6 @@ $(document).ready(() => {
         pronouns: 'she/her',
         personalUrl: 'https://elana.games',
         description: `Elana is a queer game designer and UI engineer with a passion for crafting accessible, immersive experiences to help make the world a better place. After nearly a decade in the tech industry, she now works as a Technical Designer at Bolero Game Studio. Along the way, she has also consulted for a women-owned escape room; volunteered with Games For Change; built an immersive multimedia archive to collect & preserve player stories from the <i>Myst</i> franchise; and exhibited games at Wonderville, Waffle Games, and Playcrafting NYC.`
-        //`Elana is a UI engineer & game designer, with a focus on social impact, accessibility, and queer narratives. Her career path began in the tech industry, where she worked in web development for nearly a decade. However, her creative passion has always taken her back to games - from interning for Scopely during her college years, to , to developing an arcade game for Wonderville in Queens, NY. She now works as a Technical Designer at Bolero Game Studio and Unquiet Games.`
     };
     const natData = {
         imgSrc: 'uqg_nat.png',
@@ -65,15 +64,7 @@ $(document).ready(() => {
 function onScroll(scrollAmount) {
     scrollAmount = scrollAmount || $('body').scrollTop();
 
-    // $('#logo').css('width', Math.max(700 - scrollAmount * 1.1, 160));
     $('#gradient-bg').css('opacity', Math.max((500 - scrollAmount) / 500, 0));
+    $('#logo').css('opacity', Math.max((1500 - scrollAmount) / 500, 0));
     $('#navbar').css('opacity', 1 - Math.max((900 - scrollAmount) / 500, 0));
-    // $('#wasi-name').css({
-    //     opacity: Math.max(140 - scrollAmount, 0) / 140,
-    //     height: Math.max(106 - scrollAmount * .3, 0),
-    // });
-    // $('#welcome-text').css({
-    //     opacity: Math.max(440 - scrollAmount, 0) / 175,
-    // });
-    // $('#navbar').css('opacity', Math.min(1, (scrollAmount - 200) / 100));
 }
