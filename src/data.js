20af0504928b285a900c1a93ed02e639f743a33e
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2024-06-16 14:38:33 -0400

    Update Elana's bio; move bios into separate data file; update banner image for Banned Together; fix scroll handling on mobile

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const elanaData = {
+    imgSrc: 'uqg_elana.png',
+    name: 'Elana Bell Bogdan',
+    pronouns: 'she/her',
+    personalUrl: 'https://elana.games',
+    description: `Elana is a queer game designer and UI engineer with a passion for crafting accessible, immersive experiences to help make the world a better place.  After nearly a decade in the tech industry, she now happily splits her time between Unquiet Games and a Technical Design role at <a href="https://bolerogamestudio.com/" target="_blank">Bolero Game Studio</a>. Along the way, she has consulted for a women-owned escape room; spoken on a women-in-games panel at Waffle Games; developed an arcade installation for Wonderville; volunteered with Games For Change; and helped preserve indie game history as Creative Technologist for <a href="https://vault.themystdocumentary.com">The Myst Documentary</a>.`
+};
+const natData = {
+    imgSrc: 'uqg_nat.png',
+    name: 'Nat Mesnard',
+    pronouns: 'they/them',
+    personalUrl: 'https://natmesnard.com',
+    description: `Nat writes fiction, teaches storytelling, and designs games. They are faculty at Pratt Institute and School of Visual Arts, and co-founded <a href="https://scryptidgames.com/" target="_blnak">Scryptid Games</a>. Nat has published poetry, fiction, and nonfiction in Vol. 1 Brooklyn, Cartridge Lit, Autostraddle, Bodega, Blackbird, Kenyon Review Online, The Gettysburg Review, Ninth Letter, The Journal, Green Mountains Review and elsewhere. New games include a tabletop RPG, Ball of the Wild, funded on Kickstarter, and a short RPG titled Adventurer’s Respite forthcoming in the 2023 Level 1 Anthology.`
+};
+
+export {elanaData, natData};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './bio.less';
 import './mobile.less';
 
 import BioTemplate from './templates/bio.handlebars';
+import {elanaData, natData} from './data';
 
 import '../img/uqg_logo.png';
 import '../img/uqg_logo_bg.png';
@@ -28,7 +29,7 @@ $(document).ready(() => {
     $scrollIndicator = $('#scroll-indicator');
 
     $body.addClass('animated-transitions');
-    $(window).on('resize scroll', (e) => {
+    $(window).on('resize scroll touchmove', (e) => {
         $body.removeClass('animated-transitions');
         if (e.type === 'scroll') {
             onScroll();
@@ -48,22 +49,6 @@ $(document).ready(() => {
     setTimeout(() => {
         $body.addClass('show-logo');
     }, 500);
-
-
-    const elanaData = {
-        imgSrc: 'uqg_elana.png',
-        name: 'Elana Bell Bogdan',
-        pronouns: 'she/her',
-        personalUrl: 'https://elana.games',
-        description: `Elana is a queer game designer and UI engineer with a passion for crafting accessible, immersive experiences to help make the world a better place. After nearly a decade in the tech industry, she now works as a Technical Designer at Bolero Game Studio. Along the way, she has also consulted for a women-owned escape room; volunteered with Games For Change; built an immersive multimedia archive to collect & preserve player stories from the <i>Myst</i> franchise; and exhibited games at Wonderville, Waffle Games, and Playcrafting NYC.`
-    };
-    const natData = {
-        imgSrc: 'uqg_nat.png',
-        name: 'Nat Mesnard',
-        pronouns: 'they/them',
-        personalUrl: 'https://natmesnard.com',
-        description: `Nat writes fiction, teaches storytelling, and designs games. They are faculty at Pratt Institute and School of Visual Arts, and co-founded Scryptid Games. Nat has published poetry, fiction, and nonfiction in Vol. 1 Brooklyn, Cartridge Lit, Autostraddle, Bodega, Blackbird, Kenyon Review Online, The Gettysburg Review, Ninth Letter, The Journal, Green Mountains Review and elsewhere. New games include a tabletop RPG, Ball of the Wild, funded on Kickstarter, and a short RPG titled Adventurer’s Respite forthcoming in the 2023 Level 1 Anthology.`
-    };
 
     $('#team-wrapper').html(BioTemplate(elanaData) + BioTemplate(natData));
 });
